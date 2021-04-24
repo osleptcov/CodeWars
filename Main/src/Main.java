bb30d0b0b98e329a086e258e44128d19d1d60c30
Oleg Sleptsov <oleg_sleptsov@epam.com>
2021-04-24 20:04:11 +0300

    add Create Phone Number method

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,10 @@
+import javax.swing.text.MaskFormatter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.DoubleToIntFunction;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -32,10 +36,17 @@ public class Main {
         System.out.println("Hello world!!!");
 
         System.out.println(bmi(80, 1.80));
-        long [] result = Bonus.bonus(new int[] {22, 3, 15}, 18228);
-        for (int i=0;i< result.length;i++){
-            System.out.println(result[i]);
-        }
+//        long [] result = Bonus.bonus(new int[] {22, 3, 15}, 18228);
+//        for (int i=0;i< result.length;i++){
+//            System.out.println(result[i]);
+        //}
+
+        int v[] =  {9,2,0,9,8,8,3,6,7,5};
+        String number = Arrays.toString(v).replaceAll(", |\\[|\\]", "");
+        System.out.println(number);
+        System.out.printf("(%s) %s-%s", number.substring(0, 3), number.substring(3, 6),number.substring(6, 10));
+
+
     }
 
 
