f87f20f4572a5afb2fe993fce979d26b0e70281a
Oleg Sleptsov <oleg_sleptsov@epam.com>
2021-05-05 17:14:47 +0300

    Add Solution class for Ackermann Function

---
+++ Main/src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if((m > 0) && (n == 0))
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else if((m > 0) && (n > 0))
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }else
+            return n + 1;
+    }
+}
